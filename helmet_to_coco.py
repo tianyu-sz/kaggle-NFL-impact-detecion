5b60879705e0e84833c5fb5322a3e9456583f1f0
tianyu-sz <tian.yu5@icloud.com>
2020-12-31 11:58:37 +0800

    格式化代码, 20折改10折

--- helmet_to_coco.py
+++ helmet_to_coco.py
@@ -3,7 +3,7 @@ import pandas as pd
 import os
 import platform
 
-if platform.system() =="Windows":
+if platform.system() == "Windows":
     data_root_dir = r'D:/IdeaProjects/Kaggle/kaggle-NFL-impact-detecion/'
 else:
     data_root_dir = r'/content/data/nfl-impact-detection/'
@@ -14,46 +14,46 @@ df['x'] = df['left']
 df['y'] = df['top']
 df['w'] = df["width"]
 df["h"] = df['height']
-df['x_center'] = df['x'] + df['w']/2
-df['y_center'] = df['y'] + df['h']/2
+df['x_center'] = df['x'] + df['w'] / 2
+df['y_center'] = df['y'] + df['h'] / 2
 df['classes'] = 0
 
 # 归一化
-df['x'] = df['x']/1280
-df['y'] = df['y']/720
-df['w'] = df['w']/1280
+df['x'] = df['x'] / 1280
+df['y'] = df['y'] / 720
+df['w'] = df['w'] / 1280
 df["h"] = df["h"] / 720
-df['x_center'] =  df['x_center'] / 1280
-df['y_center'] = df['y_center']/ 720
-
+df['x_center'] = df['x_center'] / 1280
+df['y_center'] = df['y_center'] / 720
 
 from tqdm.auto import tqdm
 import shutil as sh
-df = df[['image_id','x', 'y', 'w', 'h','x_center','y_center','classes']]
+
+df = df[['image_id', 'x', 'y', 'w', 'h', 'x_center', 'y_center', 'classes']]
 
 # k折数定义
-fold_count = 20     # 5 20
-fold_numbers = [18]     # 0 18
+fold_count = 10  # 5 20 10
+fold_numbers = [9]  # 0 18
 
 index = list(set(df.image_id))
 source = 'images'
 if True:
     for fold in fold_numbers:
-        val_index = index[len(index)*fold//fold_count:len(index)*(fold+1)//fold_count]
-        for name,mini in tqdm(df.groupby('image_id')):
+        val_index = index[len(index) * fold // fold_count:len(index) * (fold + 1) // fold_count]
+        for name, mini in tqdm(df.groupby('image_id')):
             if name in val_index:
                 path2save = 'val2017/'
             else:
                 path2save = 'train2017/'
-            if not os.path.exists('convertor/fold{}/labels/'.format(fold)+path2save):
-                os.makedirs('convertor/fold{}/labels/'.format(fold)+path2save)
-            with open('convertor/fold{}/labels/'.format(fold)+path2save+name+".txt", 'w+') as f:
-                row = mini[['classes','x_center','y_center','w','h']].astype(float).values
+            if not os.path.exists('convertor/fold{}/labels/'.format(fold) + path2save):
+                os.makedirs('convertor/fold{}/labels/'.format(fold) + path2save)
+            with open('convertor/fold{}/labels/'.format(fold) + path2save + name + ".txt", 'w+') as f:
+                row = mini[['classes', 'x_center', 'y_center', 'w', 'h']].astype(float).values
                 row = row.astype(str)
                 for j in range(len(row)):
                     text = ' '.join(row[j])
                     f.write(text)
                     f.write("\n")
-            if not os.path.exists('convertor/fold{}/images/{}'.format(fold,path2save)):
-                os.makedirs('convertor/fold{}/images/{}'.format(fold,path2save))
-            sh.copy((data_root_dir + r"{}/{}.jpg").format(source,name),'convertor/fold{}/images/{}/{}.jpg'.format(fold,path2save,name))
+            if not os.path.exists('convertor/fold{}/images/{}'.format(fold, path2save)):
+                os.makedirs('convertor/fold{}/images/{}'.format(fold, path2save))
+            sh.copy((data_root_dir + r"{}/{}.jpg").format(source, name), 'convertor/fold{}/images/{}/{}.jpg'.format(fold, path2save, name))
